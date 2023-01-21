123c0ae80de7670c7bc06b27ec65f9f86eb9afc1
Ciza2596 <Ciza2596@gmail.com>
2023-01-21 20:29:55 +0800

    [SaveLoadModule] Update

--- Assets/Examples/SaveLoadModule/Extension1 ZenjectInstaller/Script/Installer/StreamProvider/FileStreamProviderMonoInstaller.cs
+++ Assets/Examples/SaveLoadModule/Extension1 ZenjectInstaller/Script/Installer/StreamProvider/FileStreamProviderMonoInstaller.cs
@@ -1,16 +1,11 @@
 using SaveLoadModule;
 using SaveLoadModule.Implement;
-using UnityEngine;
 using Zenject;
 
 public class FileStreamProviderMonoInstaller : MonoInstaller
 {
-    [SerializeField] private SaveLoadModuleConfig _saveLoadModuleConfig;
-
     public override void InstallBindings()
     {
         Container.Bind<IStreamProvider>().To<FileStreamProvider>().AsSingle();
-        Container.Bind<IFileStreamProviderConfig>().To<SaveLoadModuleConfig>()
-            .FromInstance(_saveLoadModuleConfig);
     }
 }

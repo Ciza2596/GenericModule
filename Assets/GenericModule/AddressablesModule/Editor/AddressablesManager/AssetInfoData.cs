75e3e6bc22c6baac367962f1e174546eb4b14ff9
Ciza2596 <Ciza2596@gmail.com>
2023-02-26 17:24:14 +0800

    [AudioModule] Update Editor

--- Assets/GenericModule/AddressablesModule/Editor/AddressablesManager/AssetInfoData.cs
+++ Assets/GenericModule/AddressablesModule/Editor/AddressablesManager/AssetInfoData.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+
 
 namespace AddressablesModule.Editor
 {
--- Assets/GenericModule/AudioModule/Editor/CreateAudioPrefabEditor.cs
+++ Assets/GenericModule/AudioModule/Editor/CreateAudioPrefabEditor.cs
@@ -13,11 +13,41 @@ namespace AudioModule.Editor
 
         private const string CREATE_AUDIO_PREFAB_EDITOR = "CreateAudioPrefabEditor.";
 
+        
+        private readonly string _audioMixerGuidKey = $"{CREATE_AUDIO_PREFAB_EDITOR}{nameof(AudioMixerGuid)}";
+        private string AudioMixerGuid
+        {
+            get => PlayerPrefs.GetString(_audioMixerGuidKey);
+            set
+            {
+                PlayerPrefs.SetString(_audioMixerGuidKey, value);
+                PlayerPrefs.Save();
+            }
+        }
+        
+        
         private AudioMixer _audioMixer;
+        private AudioMixer AudioMixer
+        {
+            get
+            {
+                if (_audioMixer is null)
+                    _audioMixer = GetObject<AudioMixer>(AudioMixerGuid);
+
+                return _audioMixer;
+            }
+            set
+            {
+                _audioMixer = value;
+                var guid = _audioMixer is null
+                    ? string.Empty
+                    : AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(_audioMixer));
+                AudioMixerGuid = guid;
+            }
+        }
 
 
         private readonly string _volumeParameterKey = $"{CREATE_AUDIO_PREFAB_EDITOR}{nameof(VolumeParameter)}";
-
         private string VolumeParameter
         {
             get => PlayerPrefs.GetString(_volumeParameterKey);
@@ -30,7 +60,6 @@ namespace AudioModule.Editor
 
 
         private readonly string _isPlayOnAwakeKey = $"{CREATE_AUDIO_PREFAB_EDITOR}{nameof(IsPlayOnAwake)}";
-
         private bool IsPlayOnAwake
         {
             get => PlayerPrefs.GetInt(_isPlayOnAwakeKey) == 1;
@@ -42,7 +71,6 @@ namespace AudioModule.Editor
         }
 
         private readonly string _isLoopKey = $"{CREATE_AUDIO_PREFAB_EDITOR}{nameof(IsLoop)}";
-
         private bool IsLoop
         {
             get => PlayerPrefs.GetInt(_isLoopKey) == 1;
@@ -55,7 +83,6 @@ namespace AudioModule.Editor
 
 
         private readonly string _spatialBlendKey = $"{CREATE_AUDIO_PREFAB_EDITOR}{nameof(SpatialBlend)}";
-
         private float SpatialBlend
         {
             get => PlayerPrefs.GetFloat(_spatialBlendKey);
@@ -68,7 +95,6 @@ namespace AudioModule.Editor
 
 
         private readonly string _audioPrefabFolderPathKey = $"{CREATE_AUDIO_PREFAB_EDITOR}{nameof(AudioPrefabFolderPath)}";
-
         private string AudioPrefabFolderPath
         {
             get
@@ -85,11 +111,40 @@ namespace AudioModule.Editor
         }
 
 
+        private readonly string _audioClipGuidKey = $"{CREATE_AUDIO_PREFAB_EDITOR}{nameof(AudioClipGuid)}";
+        private string AudioClipGuid
+        {
+            get => PlayerPrefs.GetString(_audioClipGuidKey);
+            set
+            {
+                PlayerPrefs.SetString(_audioClipGuidKey, value);
+                PlayerPrefs.Save();
+            }
+        }
+        
+        
         private AudioClip _audioClip;
+        private AudioClip AudioClip
+        {
+            get
+            {
+                if (_audioClip is null)
+                    _audioClip = GetObject<AudioClip>(AudioClipGuid);
+
+                return _audioClip;
+            }
+            set
+            {
+                _audioClip = value;
+                var guid = _audioClip is null
+                    ? string.Empty
+                    : AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(_audioClip));
+                AudioClipGuid = guid;
+            }
+        }
 
 
         private readonly string _audioClipFolderPathKey = $"{CREATE_AUDIO_PREFAB_EDITOR}{nameof(AudioClipFolderPath)}";
-
         private string AudioClipFolderPath
         {
             get
@@ -134,15 +189,15 @@ namespace AudioModule.Editor
 
         private void SettingsArea()
         {
-            _audioMixer =
-                EditorGUILayout.ObjectField("AudioMixer", _audioMixer, typeof(AudioMixer), true) as AudioMixer;
+            AudioMixer =
+                EditorGUILayout.ObjectField("AudioMixer", AudioMixer, typeof(AudioMixer), true) as AudioMixer;
             VolumeParameter = EditorGUILayout.TextField("volumeParameter", VolumeParameter);
 
             EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
 
             IsPlayOnAwake = EditorGUILayout.Toggle("isPlayOnAwake", IsPlayOnAwake);
             IsLoop = EditorGUILayout.Toggle("isLoop", IsLoop);
-            SpatialBlend = EditorGUILayout.FloatField("spatialBlend", SpatialBlend);
+            SpatialBlend = EditorGUILayout.Slider("spatialBlend", SpatialBlend,0,1);
 
             EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
 
@@ -159,12 +214,12 @@ namespace AudioModule.Editor
 
         private void CreateAudioPrefabArea()
         {
-            _audioClip = EditorGUILayout.ObjectField(_audioClip, typeof(AudioClip), true) as AudioClip;
+            AudioClip = EditorGUILayout.ObjectField(AudioClip, typeof(AudioClip), true) as AudioClip;
 
             EditorGUILayout.Space();
 
             if (GUILayout.Button("Create Audio Prefab"))
-                CreateAudioPrefab(_audioClip);
+                CreateAudioPrefab(AudioClip);
         }
 
         private void CreateAudioPrefabsArea()
@@ -199,7 +254,7 @@ namespace AudioModule.Editor
             var audioSource = audioGameObject.AddComponent<AudioSource>();
             audioSource.clip = audioClip;
 
-            audioSource.outputAudioMixerGroup = _audioMixer.FindMatchingGroups(VolumeParameter)[0];
+            audioSource.outputAudioMixerGroup = AudioMixer.FindMatchingGroups(VolumeParameter)[0];
 
             audioSource.playOnAwake = IsPlayOnAwake;
             audioSource.loop = IsLoop;
@@ -221,8 +276,7 @@ namespace AudioModule.Editor
         {
             prefabName += ".prefab";
             var path = Path.Combine(AudioPrefabFolderPath, prefabName);
-            var localPath = AssetDatabase.GenerateUniqueAssetPath(path);
-            return localPath;
+            return path;
         }
 
         private string GetFolderPathAndOpenWindow(string label, string originPath)
@@ -231,10 +285,42 @@ namespace AudioModule.Editor
             {
                 var path = EditorGUILayout.TextField(label, originPath);
                 if (GUILayout.Button("Select", EditorStyles.miniButton, GUILayout.Width(65)))
-                    path = EditorUtility.OpenFolderPanel("Folder Path", "", "");
+                {
+                    path = EditorUtility.OpenFolderPanel("Folder Path", originPath, "");
+                    path = GetAssetPathWithoutDataPath(path);
+                }
 
                 return string.IsNullOrWhiteSpace(path) ? originPath : path;
             }
+        }
+        
+        private T GetObject<T>(string guid) where T : Object
+        {
+            var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            var obj = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+            return obj;
+        }
+        
+        private string GetAssetPathWithoutDataPath(string path)
+        {
+            var dataPath = Application.dataPath;
+            dataPath = dataPath.Replace("Assets", "");
+            
+            if(path.Contains(dataPath)) 
+                path = path.Replace(dataPath, "");
+
+            return path;
+        }
+
+        private string GetAssetPathWithDataPath(string path)
+        {
+            var dataPath = Application.dataPath;
+            dataPath = dataPath.Replace("Assets", "");
+
+            if (!path.Contains(dataPath))
+                path = dataPath + path;
+
+            return path;
         }
     }
 }

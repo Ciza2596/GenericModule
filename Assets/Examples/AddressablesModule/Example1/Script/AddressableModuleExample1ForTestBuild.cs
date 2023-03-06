b44682446ae8348f432133a9f401c136dcbf87d0
Ciza2596 <Ciza2596@gmail.com>
2023-03-07 00:13:36 +0800

    Update Version 0.0.2

---
+++ Assets/Examples/AddressablesModule/Example1/Script/AddressableModuleExample1ForTestBuild.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AddressablesModule.Example1
+{
+    public class AddressableModuleExample1ForTestBuild : MonoBehaviour
+    {
+        [SerializeField] private string _address = "DUNE_1";
+        [Space] [SerializeField] private AddressMapListDataOverview _addressMapListDataOverview;
+        [SerializeField] private Image _image;
+
+        [Space] [SerializeField] private Button _loadButton;
+        [SerializeField] private Button _unloadButton;
+
+        private AddressablesModule _addressablesModule;
+
+
+        //unity callback
+
+        private void Awake()
+        {
+            _addressablesModule = new AddressablesModule();
+
+            _loadButton.onClick.AddListener(OnLoadButtonClick);
+            _unloadButton.onClick.AddListener(OnUnloadButtonClick);
+        }
+
+        //private method
+        private async void OnLoadButtonClick() =>
+            _image.sprite = await _addressablesModule.LoadAssetAsync<Sprite>(_address);
+
+
+        private void OnUnloadButtonClick() =>
+            _addressablesModule.UnloadAsset(_address, typeof(Sprite));
+    }
+}
--- Assets/Examples/AddressablesModule/Example1/Script/LoadingEntry.cs
+++ Assets/Examples/AddressablesModule/Example1/Script/LoadingEntry.cs
@@ -7,6 +7,7 @@ namespace AddressablesModule.Example1
     public class LoadingEntry : MonoBehaviour
     {
         //private variable
+        
         [SerializeField] private AddressMapListDataOverview _addressMapListDataOverview;
         [SerializeField] private Image[] _images;
 
@@ -22,29 +23,29 @@ namespace AddressablesModule.Example1
 
         private async void OnEnable()
         {
-            // _images[0].sprite = await _addressablesModule.LoadAssetAsync<Sprite>("DUNE_1");
+            _images[0].sprite = await _addressablesModule.LoadAssetAsync<Sprite>("DUNE_1");
 
 
-            var addressMapList = _addressMapListDataOverview.GetAddressMapList();
-
-            await _addressablesModule.LoadAssetsAsync(addressMapList);
-            
-            var length = addressMapList.Length;
-            for (int i = 0; i < length; i++)
-            {
-                var addressObjectTypeMap = addressMapList[i];
-                var address = addressObjectTypeMap.Address;
-            
-                var sprite = _addressablesModule.GetAsset<Sprite>(address);
-            
-                var image = _images[i];
-                image.sprite = sprite;
-            }
+            // var addressMapList = _addressMapListDataOverview.GetAddressMapList();
+            //
+            // await _addressablesModule.LoadAssetsAsync(addressMapList);
+            //
+            // var length = addressMapList.Length;
+            // for (int i = 0; i < length; i++)
+            // {
+            //     var addressObjectTypeMap = addressMapList[i];
+            //     var address = addressObjectTypeMap.Address;
+            //
+            //     var sprite = _addressablesModule.GetAsset<Sprite>(address);
+            //
+            //     var image = _images[i];
+            //     image.sprite = sprite;
+            // }
         }
 
         private void OnDisable()
         {
-            // _addressablesModule.UnloadAsset("DUNE_1", typeof(Sprite));
+            _addressablesModule.UnloadAsset("DUNE_1", typeof(Sprite));
             //_addressablesModule.UnloadAllAssets();
         }
     }

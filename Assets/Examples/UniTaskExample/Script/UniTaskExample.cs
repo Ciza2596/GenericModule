0aa1796ec69ec765344b5d30f328a9a175ac0034
Ciza2596 <Ciza2596@gmail.com>
2023-03-08 22:10:23 +0800

    Add UniTaskExample, [PageModule] remove editor folder

---
+++ Assets/Examples/UniTaskExample/Script/UniTaskExample.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class UniTaskExample : MonoBehaviour
+{
+    [SerializeField] private float _delaySeconds = 1;
+
+    private CancellationTokenSource _cancellationTokenSource;
+
+
+    [Button]
+    private async void Show()
+    {
+        Debug.Log("ShowStart.");
+
+        await PlayAnim();
+
+        Debug.Log("ShowEnd.");
+    }
+
+
+    [Button]
+    private void Cancel()
+    {
+        _cancellationTokenSource.Cancel();
+    }
+
+    private async UniTask PlayAnim()
+    {
+        if (_cancellationTokenSource != null)
+            _cancellationTokenSource.Cancel();
+
+        _cancellationTokenSource = new CancellationTokenSource();
+
+        Debug.Log("PlayAnimStart.");
+        try
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(_delaySeconds), cancellationToken: _cancellationTokenSource.Token);
+        }
+        catch
+        {
+            // ignored
+        }
+
+        Debug.Log("PlayAnimEnd.");
+    }
+}

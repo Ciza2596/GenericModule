5c43dd8d3a2b42eadd8575b49d7d3af467fd18fe
Ciza2596 <Ciza2596@gmail.com>
2023-04-05 13:37:48 +0800

    Add ObjectPoolModule

---
+++ Assets/GenericModule/ObjectPoolModule/Test/ObjectPoolModuleTest.cs
@@ -0,0 +1,89 @@
+
+
+using System;
+using CizaObjectPoolModule;
+using NUnit.Framework;
+
+public class ObjectPoolModuleTest
+{
+
+    private ObjectPoolModule<FakeObject> _fakeObjectPool; 
+
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _fakeObjectPool = new ObjectPoolModule<FakeObject>();
+    }
+
+
+    [Test]
+    public void Call_Spawn_Return_Obj()
+    {
+        // act
+        var fakeObject = _fakeObjectPool.Spawn();
+        
+        // assert
+        Assert.AreEqual(_fakeObjectPool.UsingCount,1);
+        Assert.AreEqual(_fakeObjectPool.UnusingCount,0);
+        Assert.IsNotNull(fakeObject, "Object cant be null");
+    }
+
+    [Test]
+    public void Call_DeSpawn_Recycle_Obj()
+    {
+
+        // arrange
+        var fakeObject = Spawn_Obj_And_Check();
+        Assert.IsFalse(fakeObject.IsDispose);
+        
+        // act
+        _fakeObjectPool.DeSpawn(fakeObject);
+        
+        // assert
+        Assert.AreEqual(_fakeObjectPool.UsingCount,0);
+        Assert.AreEqual(_fakeObjectPool.UnusingCount,1);
+        Assert.IsTrue(fakeObject.IsDispose);
+    }
+    
+    [Test]
+    public void Call_Release_Clear_Pool()
+    {
+
+        // arrange
+        var fakeObject = Spawn_Obj_And_Check();
+        Assert.IsFalse(fakeObject.IsDispose);
+        
+        // act
+        _fakeObjectPool.Release();
+        
+        // assert
+        Assert.AreEqual(_fakeObjectPool.UsingCount,0);
+        Assert.AreEqual(_fakeObjectPool.UnusingCount,0);
+        Assert.IsTrue(fakeObject.IsDispose);
+    }
+    
+    
+    
+    private FakeObject Spawn_Obj_And_Check()
+    {
+        var fakeObject = _fakeObjectPool.Spawn();
+        
+        Assert.AreEqual(_fakeObjectPool.UsingCount,1);
+        Assert.AreEqual(_fakeObjectPool.UnusingCount,0);
+        Assert.IsNotNull(fakeObject, "Object cant be null");
+
+        return fakeObject;
+    }
+    
+    private class FakeObject: IDisposable
+    {
+        public bool IsDispose { get; private set; }
+
+
+        public void Dispose()
+        {
+            IsDispose = true;
+        }
+    }
+}

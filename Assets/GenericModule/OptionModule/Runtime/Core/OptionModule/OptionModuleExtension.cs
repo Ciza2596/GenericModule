9cf48a5358d05a9b07badcfd4c9d9e5b0bc1b47a
Ciza2596 <Ciza2596@gmail.com>
2023-11-15 15:59:03 +0800

    [OptionModule] Update to 0.0.6

--- Assets/GenericModule/OptionModule/Runtime/Core/OptionModule/OptionModuleExtension.cs
+++ Assets/GenericModule/OptionModule/Runtime/Core/OptionModule/OptionModuleExtension.cs
@@ -5,20 +5,54 @@ namespace CizaOptionModule
 {
 	public static class OptionModuleExtension
 	{
-		public static async void MovePageToLeft(this OptionModule optionModule, int playerIndex)
+		public static UniTask MovePageToLeftAsync(this OptionModule optionModule, int playerIndex)
 		{
 			if (optionModule.IsChangingPage)
-				return;
+				return UniTask.CompletedTask;
 
-			await optionModule.TryMovePageToLeftAsync(playerIndex);
+			return optionModule.TryMovePageToLeftAsync(playerIndex);
 		}
 
-		public static async void MovePageToRight(this OptionModule optionModule, int playerIndex)
+		public static UniTask MovePageToRightAsync(this OptionModule optionModule, int playerIndex)
+		{
+			if (optionModule.IsChangingPage)
+				return UniTask.CompletedTask;
+
+			return optionModule.TryMovePageToRightAsync(playerIndex);
+		}
+
+		public static async UniTask HorizontalMovementAsync(this OptionModule optionModule, int playerIndex, Vector2 direction)
 		{
 			if (optionModule.IsChangingPage)
 				return;
 
-			await optionModule.TryMovePageToRightAsync(playerIndex);
+			if (direction.x > 0)
+			{
+				if (await optionModule.TryMoveOptionToRightAsync(playerIndex))
+					return;
+			}
+			else if (direction.x < 0)
+			{
+				if (await optionModule.TryMoveOptionToLeftAsync(playerIndex))
+					return;
+			}
+		}
+
+		public static async UniTask VerticalMovementAsync(this OptionModule optionModule, int playerIndex, Vector2 direction)
+		{
+			if (optionModule.IsChangingPage)
+				return;
+
+			if (direction.y > 0)
+			{
+				if (optionModule.TryMoveOptionToUp(playerIndex))
+					return;
+			}
+			else if (direction.y < 0)
+			{
+				if (optionModule.TryMoveOptionToDown(playerIndex))
+					return;
+			}
 		}
 
 		public static async UniTask MovementAsync(this OptionModule optionModule, int playerIndex, Vector2 direction)

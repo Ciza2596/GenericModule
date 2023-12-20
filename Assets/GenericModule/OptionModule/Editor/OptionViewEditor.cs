bd9ddf526934a136a2c74bb64d6dbdaa9b840d0b
Ciza2596 <Ciza2596@gmail.com>
2023-12-20 15:54:36 +0800

    [OptionModule] 0.0.32

--- Assets/GenericModule/OptionModule/Editor/OptionViewEditor.cs
+++ Assets/GenericModule/OptionModule/Editor/OptionViewEditor.cs
@@ -3,34 +3,40 @@ using UnityEditor;
 
 namespace OptionModule.Editor
 {
-	[CustomEditor(typeof(OptionView))]
-	[CanEditMultipleObjects]
-	public class OptionViewEditor : UnityEditor.Editor
-	{
-		private SerializedProperty _collectionSettings;
+    [CustomEditor(typeof(OptionView))]
+    [CanEditMultipleObjects]
+    public class OptionViewEditor : UnityEditor.Editor
+    {
+        private SerializedProperty _collectionSettings;
 
-		private SerializedProperty _isUseAnim;
-		private SerializedProperty _animSettings;
+        private SerializedProperty _isUseAnim;
+        private SerializedProperty _animSettings;
 
-		private void OnEnable()
-		{
-			_collectionSettings = serializedObject.FindProperty(nameof(_collectionSettings));
+        private SerializedProperty _optionViewSups;
 
-			_isUseAnim    = serializedObject.FindProperty(nameof(_isUseAnim));
-			_animSettings = serializedObject.FindProperty(nameof(_animSettings));
-		}
+        private void OnEnable()
+        {
+            _collectionSettings = serializedObject.FindProperty(nameof(_collectionSettings));
 
-		public override void OnInspectorGUI()
-		{
-			serializedObject.Update();
+            _isUseAnim = serializedObject.FindProperty(nameof(_isUseAnim));
+            _animSettings = serializedObject.FindProperty(nameof(_animSettings));
 
-			EditorGUILayout.PropertyField(_collectionSettings);
-			EditorGUILayout.PropertyField(_isUseAnim);
+            _optionViewSups = serializedObject.FindProperty(nameof(_optionViewSups));
+        }
 
-			if (_isUseAnim.boolValue)
-				EditorGUILayout.PropertyField(_animSettings);
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
 
-			serializedObject.ApplyModifiedProperties();
-		}
-	}
+            EditorGUILayout.PropertyField(_collectionSettings);
+            EditorGUILayout.PropertyField(_isUseAnim);
+
+            if (_isUseAnim.boolValue)
+                EditorGUILayout.PropertyField(_animSettings);
+
+            EditorGUILayout.PropertyField(_optionViewSups);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
 }

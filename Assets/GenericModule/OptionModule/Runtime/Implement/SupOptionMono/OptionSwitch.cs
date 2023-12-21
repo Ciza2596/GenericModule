9d4bbeba68795a4901c2e47e2867329bb49740c0
Ciza2596 <Ciza2596@gmail.com>
2023-12-21 20:41:48 +0800

    [OptionModule] 0.0.36 Add OptionSwotch

---
+++ Assets/GenericModule/OptionModule/Runtime/Implement/SupOptionMono/OptionSwitch.cs
@@ -0,0 +1,40 @@
+using System;
+using CizaCore.UI;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CizaOptionModule.Implement
+{
+    public class OptionSwitch : OptionSup
+    {
+        [SerializeField]
+        private Switch _switch;
+
+        public event Action<bool> OnIsOnChanged;
+
+        public bool IsOn => _switch.IsOn;
+
+        public void TurnOn() =>
+            _switch.TurnOn();
+
+        public void TurnOff() =>
+            _switch.TurnOff();
+
+        public override void Initialize(Option option)
+        {
+            base.Initialize(option);
+
+            Assert.IsNotNull(_switch, $"[OptionSwitch::Initialize] Option: {name} is not set switch.");
+            _switch.OnIsOnChanged += OnIsOnChangedImp;
+        }
+
+        public override void Release(Option option)
+        {
+            base.Release(option);
+            _switch.OnIsOnChanged -= OnIsOnChangedImp;
+        }
+
+        private void OnIsOnChangedImp(bool isOn) =>
+            OnIsOnChanged?.Invoke(isOn);
+    }
+}

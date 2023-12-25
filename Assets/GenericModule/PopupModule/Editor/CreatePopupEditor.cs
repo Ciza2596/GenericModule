88116cbd4c47fe2685e49121be6cad187eb7e3a8
Ciza2596 <Ciza2596@gmail.com>
2023-12-25 16:19:28 +0800

    Add CreatePopupEditor

---
+++ Assets/GenericModule/PopupModule/Editor/CreatePopupEditor.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace CizaPopupModule.Editor
+{
+    public class CreatePopupEditor
+    {
+        public const string PopupModulePath = "PopupModule/";
+
+        public const string PopupCanvas = "PopupCanvas";
+
+        [MenuItem("GameObject/Ciza/PopupModule/PopupCanvas", false, 10)]
+        public static void CreateVerticalScrollView()
+        {
+            CreateUIObject(PopupCanvas);
+        }
+
+        public const string Popup = "Popup";
+
+        [MenuItem("GameObject/Ciza/PopupModule/Popup", false, 10)]
+        public static void CreateDropdown()
+        {
+            CreateUIObject(Popup);
+        }
+
+        private static void CreateUIObject(string dataId)
+        {
+            var prefab = Resources.Load<GameObject>(PopupModulePath + dataId);
+            var uiObject = Object.Instantiate(prefab, Selection.activeTransform);
+            uiObject.name = dataId;
+        }
+    }
+}

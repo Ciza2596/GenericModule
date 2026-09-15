fd09092a2d48610583e356cd68dd30a84c2f48b3
Ciza Huang <ciza2596@gmail.com>
2026-09-15 18:46:11 +0800

    EventSystemUtils

---
+++ Assets/Plugins/Ciza/InputModule/InputModule/Script/Runtime/Core/Common/Utils/EventSystemUtils.cs
@@ -0,0 +1,9 @@
+using UnityEngine.EventSystems;
+
+namespace CizaInputModule
+{
+	public static class EventSystemUtils
+	{
+		public static PointerEventData EmptyPointerEventData = new PointerEventData(null);
+	}
+}
